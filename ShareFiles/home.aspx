95c24b9950481d5e6acf4b74431fdd7f08e37737
Wasim Khan <wasimkhan0816@gmail.com>
2023-12-19 20:46:19 +0530

    ShareText all source code added

---
+++ ShareFiles/home.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="ShareFiles.home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #TextArea1 {
+            height: 27px;
+            width: 193px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p style="font-family: Arial; font-size: medium"> ShareText</p>
+            <p> Write your Text<asp:TextBox ID="TextBox3" runat="server" Height="31px" TextMode="MultiLine" Width="490px"></asp:TextBox>
+                <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Submit" />
+            </p>
+            <p> Generated ID <asp:TextBox ID="TextBox5" runat="server" ReadOnly="True"></asp:TextBox>
+            </p>
+            <p> &nbsp;</p>
+            <p> Enter the ID
+                <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+                <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Get Text" />
+            </p>
+            <p> Result<asp:TextBox ID="TextBox4" runat="server" Height="31px" ReadOnly="True" TextMode="MultiLine" Width="576px"></asp:TextBox>
+            </p>
+            <p> &nbsp;</p>
+            <p> 
+                <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Clear All" />
+            </p>
+            <p> &nbsp;</p>
+            <p> &nbsp;</p>
+            <hr />
+            <h1 class="article__title" style="padding: 0px 0px 20px; margin: 0px; outline: 0px; box-sizing: border-box; font-family: Arial, Helvetica, sans-serif; -webkit-font-smoothing: antialiased; font-weight: normal; line-height: 1.2; font-size: small; color: rgb(0, 0, 0); font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: normal; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial;">All right reserved © ShareFile 2023&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Developed by-
+                <asp:LinkButton ID="LinkButton1" runat="server" onclientclick="window.open('https://www.linkedin.com/in/iwasimkhan01/');"  target="_self">Wasim Khan</asp:LinkButton>
+            </h1>
+            <p> &nbsp;</p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ShareFiles/home.aspx.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace ShareFiles
+{
+    public partial class home : System.Web.UI.Page
+    {
+
+        string constr = ConfigurationManager.ConnectionStrings["connstr"].ConnectionString;
+        SqlConnection conn;
+        DataTable dttest = new DataTable();
+        string cmdstr = string.Empty;
+        string errstr = string.Empty;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(constr);
+            conn.Open();
+        }
+
+        
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string gettext = string.Empty;
+                string newstr = string.Empty;
+                //conn.Open();
+                string getid = string.Empty;
+                if (string.IsNullOrWhiteSpace(TextBox3.Text))
+                {
+
+                    //ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Please enter the text')", true);
+                    errstr = "Please enter the text";
+                    showpopupmessage(errstr);
+                    return;
+                }
+                else {
+                    gettext = TextBox3.Text;
+                    string s1 = "'";
+                    string s2 = "''";
+                     newstr = gettext.Replace(s1,s2);
+                    if (gettext.Length > 4500)
+                    {
+                        //ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Please enter 4500 character only')", true);
+                        errstr = "Please enter 4500 character only";
+                        showpopupmessage(errstr);
+                        return;
+                    }
+                    
+                }
+                
+                
+                DateTime today = DateTime.Now;
+                string updatedate = "yyyy-MM-dd HH:mm:ss";
+                cmdstr = "insert into share_files_tab (TXT_FILE_NAME,TXT_DATE_TIME)  values ('" + newstr.Trim() + "','" + today.ToString(updatedate) + "')";
+                getstoredintoDB(cmdstr);
+
+                cmdstr = "select top 1 id from share_files_tab order by id desc";
+                SqlCommand cmd = new SqlCommand(cmdstr, conn);
+                SqlDataReader sdr = cmd.ExecuteReader();
+
+                while (sdr.Read())
+                {
+                    getid = sdr["id"].ToString();
+                }
+                TextBox5.Text = getid;
+                //string err = " Error inserting the text";
+                //ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Successfully inserted and ID generated')", true);
+                errstr = "Successfully inserted and ID generated";
+                showpopupmessage(errstr);
+                sdr.Close();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                //string err = "Error inserting the text";
+                //ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('"+err+ "')" , true);
+                errstr = "Error inserting the text " + ex.Message.ToString();
+                string newerr= errstr.Replace("'","''");
+                showpopupmessage(newerr);
+
+            }
+            
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = 0;
+                string gettext = string.Empty;
+
+                if (System.Text.RegularExpressions.Regex.IsMatch(TextBox6.Text, "[^0-9]"))
+                {
+
+                    //ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Please enter numeric only')", true);
+                    errstr = "Please enter numeric only";
+                    showpopupmessage(errstr);
+                    return;
+
+                }
+                else
+                {
+                    id = Convert.ToInt32(TextBox6.Text);
+                }
+                cmdstr = "select TXT_FILE_NAME from share_files_tab where id='" + id + "'";
+                SqlCommand cmd = new SqlCommand(cmdstr, conn);
+                SqlDataReader sdr = cmd.ExecuteReader();
+
+                while (sdr.Read())
+                {
+                    gettext = sdr["TXT_FILE_NAME"].ToString();
+                }
+                if (gettext == "")
+                {
+                    
+                    //ClientScript.RegisterStartupScript(this.GetType(), "alertMessage", "alert('" + err + "')", true);
+                    errstr = "There is not data present at ID " + id + ", please enter the valid ID";
+                    showpopupmessage(errstr);
+                }
+                else
+                { TextBox4.Text = gettext; }
+
+            
+            }
+            catch (Exception)
+            {
+
+                //ClientScript.RegisterStartupScript(this.GetType(), "alertMessage", "alert('Unable to find the Data')", true);
+                errstr = "Unable to find the Data";
+                showpopupmessage(errstr);
+                return;
+            }
+        }
+
+        public void getstoredintoDB(string str)
+        {
+            SqlCommand cmd = new SqlCommand(str, conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            cmd.ExecuteNonQuery();
+            
+
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            //ProcessStartInfo url = new ProcessStartInfo("www.linkedin.com/in/iwasimkhan01/");
+            //Process.Start(url);
+           // System.Diagnostics.Process.Start("www.linkedin.com/in/iwasimkhan01/");
+            LinkButton1.Attributes.Add("href", "www.linkedin.com/in/iwasimkhan01/");
+            LinkButton1.Attributes.Add("target", "_blank");
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            TextBox3.Text = "";
+            TextBox5.Text = "";
+            TextBox6.Text = "";
+            TextBox4.Text = "";
+        }
+        public void showpopupmessage(string geterrstr) {
+            string err = geterrstr;
+            ClientScript.RegisterStartupScript(this.GetType(), "alertMessage", "alert('"+ err + "')", true);
+
+        }
+    }
+}
